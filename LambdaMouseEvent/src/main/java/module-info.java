04409d74880e1535d3bd4e1e83d2fc8569c57370
shihanwj <shihanwei495@gmail.com>
2024-10-03 13:50:18 +0200

    Delete LambdaMouseEvent directory

--- LambdaMouseEvent/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module es.ieslosmontecillos.lambdamouseevent {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens es.ieslosmontecillos.lambdamouseevent to javafx.fxml;
-    exports es.ieslosmontecillos.lambdamouseevent;
-}
